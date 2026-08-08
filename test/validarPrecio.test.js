0d3a5a158a8f09f46b9e8d62996e41d16803a50a
enriquemacias21 <enriquemacias21@hotmail.com>
2026-08-07 19:47:58 -0500

    ci: agregar pruebas automatizdas y pipeline

---
+++ test/validarPrecio.test.js
@@ -0,0 +1,10 @@
+const {validarPrecio} = require('../src/validarPrecio');
+test('validarPrecio should return true for negative numbers', () => {
+    expect(validarPrecio(-5)).toBe(true);
+});
+
+test('validarPrecio should return true for non-number inputs', () => {
+    expect(validarPrecio('string')).toBe(true);
+    expect(validarPrecio(null)).toBe(true);
+    expect(validarPrecio(undefined)).toBe(true);
+});
